e0539496967ebd53c973c39da0fd7cdd6173ba64
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-08-19 16:52:59 +0200

    add single dictionary COINS to store all coins data

--- js/settings.js
+++ js/settings.js
@@ -1,29 +1,59 @@
-export const NAME_BY_SYMBOL = { 
-    BTC: 'Bitcoin',
-    eth: 'Ethereum',
-    trx: 'Tron',
-    ltc: 'Litecoin',
-    xrp: 'XRP',
-    doge: 'Dogecoin',
-    usdt: 'Tether',
+export const COINS = {
+    BTC: {
+        id: "bitcoin",
+        symbol: "btc",
+        name: "Bitcoin",
+        icon: "../assets/img/btc.png",
+        color: "#FFA800",
+    },
+    ETH: {
+        id: "ethereum",
+        symbol: "eth",
+        name: "Ethereum",
+        icon: "../assets/img/eth.png",
+        color: "#3A6FF8",
+    },
+    TRX: {
+        id: "tron",
+        symbol: "trx",
+        name: "Tron",
+        icon: "../assets/img/trx.png",
+        color: "#FF073A",
+    },
+    LTC: {
+        id: "litecoin",
+        symbol: "ltc",
+        name: "Litecoin",
+        icon: "../assets/img/ltc.png",
+        color: "#64CFF9",
+    },
+    XRP: {
+        id: "ripple",
+        symbol: "xrp",
+        name: "XRP",
+        icon: "../assets/img/xrp.png",
+        color: "#346AA9",
+    },
+    DOGE: {
+        id: "dogecoin",
+        symbol: "doge",
+        name: "Dogecoin",
+        icon: "../assets/img/doge.png",
+        color: "#DBD42B",
+    },
+    USDT: {
+        id: "tether",
+        symbol: "usdt",
+        name: "Tether",
+        icon: "../assets/img/usdt.png",
+        color: "#129B28",
+    },
 };
 
-export const ICON_BY_SYMBOL = {
-    BTC: "../assets/img/bitcoin.png",
-    ETH: "../assets/img/ethereum.png",
-    TRX: "../assets/img/tron.png",
-    LTC: "../assets/img/litecoin.png",
-    XRP: "../assets/img/xrp.png",
-    DOGE: "../assets/img/doge.png",
-    USDT: "../assets/img/tether.png",
-};
+export const COINS_BY_ID = Object.fromEntries(
+    Object.values(COINS).map(c => [c.id, c])
+);
 
-export const COLOR_BY_SYMBOL = {
-    BTC: "#FFA800",
-    ETH: "#3A6FF8", //"#00ADEF",
-    TRX: "#FF073A",
-    LTC: "#64CFF9",
-    XRP: "#346AA9",
-    DOGE: "#DBD42B",
-    USDT: "#129B28",
-};
+export const COINS_BY_SYMBOL = Object.fromEntries(
+    Object.values(COINS).map(c => [c.symbol, c])
+);
