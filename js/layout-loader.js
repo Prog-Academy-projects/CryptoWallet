3203ac09ff97c2428e29aded9a9c672bda4d08f7
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-08-22 21:31:13 +0200

    update dashboard page to work via layout

--- js/layout-loader.js
+++ js/layout-loader.js
@@ -1,23 +1,62 @@
-
-
-async function loadLayout() {
-    console.log('layout-loader booted');
+console.log("Start")
+window.onload = async function() {
+    console.log("Loader booted!");
     const response = await fetch("../layout.html");
     const layoutHtml = await response.text();
 
     const template = document.querySelector("template");
+    if (!template) {
+        console.error("No <template> found on page!");
+        return;
+    }
     const parser = new DOMParser();
     const doc = parser.parseFromString(layoutHtml, "text/html");
 
     const caption = template.content.querySelector("[data-caption]")?.innerHTML || "";
     const content = template.content.querySelector("[data-content]")?.innerHTML || "";
-
     let finalHtml = doc.documentElement.innerHTML
+        .replace("{{title}}", template.dataset.title || "Crypto Wallet")
+        .replace("{{head}}", template.dataset.head || "")
         .replace("{{caption}}", caption)
         .replace("{{content}}", content)
-        .replace("{{scripts}}", template.dataset.scripts || "")
+        .replace("{{scripts}}", template.dataset.scripts || "");
+
+    // document.documentElement.innerHTML = finalHtml;
+
+    document.open();
+    document.write(finalHtml);
+    document.close();
+
+    const section = template.dataset.section;
+    if (section) {
+    document
+        .querySelector(`[data-section="${section}"]`)
+        ?.classList.add("active-cw");
+    }        
+};
+
+async function loadLayout() {
+    console.log('layout-loader booted');
+
+    const response = await fetch("../layout.html");
+    const layoutHtml = await response.text();
+
+    const template = document.querySelector("template");
+    if (!template) {
+        console.error("No <template> found on page!");
+        return;
+    }
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(layoutHtml, "text/html");
+
+    const caption = template.content.querySelector("[data-caption]")?.innerHTML || "";
+    const content = template.content.querySelector("[data-content]")?.innerHTML || "";
+    let finalHtml = doc.documentElement.innerHTML
+        .replace("{{title}}", template.dataset.title || "Crypto Wallet")
         .replace("{{head}}", template.dataset.head || "")
-        .replace("{{title}}", template.dataset.title || "Crypto Wallet");
+        .replace("{{caption}}", caption)
+        .replace("{{content}}", content)
+        .replace("{{scripts}}", template.dataset.scripts || "");
 
     // document.documentElement.innerHTML = finalHtml;
 
@@ -33,29 +72,4 @@ async function loadLayout() {
     }
 }
 
-loadLayout();
-
-
-async function applyLayout() {
-  const template = document.querySelector("template");
-
-  const resp = await fetch("../layout.html");
-  let html = await resp.text();
-
-  html = html.replace("{{title}}", template.dataset.title || "App");
-  html = html.replace("{{head}}", template.dataset.head || "");
-  html = html.replace("{{content}}", template.innerHTML);
-  html = html.replace("{{scripts}}", template.dataset.scripts || "");
-
-  document.open();
-  document.write(html);
-  document.close();
-
-  const section = template.dataset.section;
-  if (section) {
-    document
-      .querySelector(`[data-section="${section}"]`)
-      ?.classList.add("active-cw");
-  }
-}
-// applyLayout();
+// loadLayout();
