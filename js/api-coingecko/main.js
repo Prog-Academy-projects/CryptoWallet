b7bcf83cc7505141f91cf42732b3e33b74e9607e
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-08-31 14:26:42 +0200

    edit url in main.js

--- js/api-coingecko/main.js
+++ js/api-coingecko/main.js
@@ -1,4 +1,4 @@
-import { API_KEY } from "./config";
+import { API_KEY } from "../config.js";
 
 export const URL = 'https://api.coingecko.com/api/v3/'
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
